c85c74bd983deae82bce22fe78856266329ef0c2
alexr <alexramirez92606@gmail.com>
2026-04-15 11:45:51 -0500

    final commit

--- src/main/java/WhileLoop.java
+++ src/main/java/WhileLoop.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class WhileLoop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count <5) {
+            System.out.println("I Love Java");
+            count++;
+        }
+    }
 }
